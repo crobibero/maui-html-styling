7035217e15777d641e4a2238e9fa95ed05016ad1
Cody Robibero <cody@robibe.ro>
2024-02-05 15:38:09 -0700

    inline html

--- MauiApp1/MainPage.xaml.cs
+++ MauiApp1/MainPage.xaml.cs
@@ -5,17 +5,5 @@ public partial class MainPage : ContentPage
     public MainPage()
     {
         InitializeComponent();
-
-        var str = """
-                    <strong>Bold Text</strong><br>
-                    <em>italics</em><br>
-                    <ul>
-                      <li>Item 1</li>
-                      <li>Item 2</li>
-                    </ul>
-                    """;
-
-        Label1.Text = str;
-        Label2.Text = str;
     }
 }
