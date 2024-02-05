b591ebdd7cd3f17a643faaa7638de1758d319297
Cody Robibero <cody@robibe.ro>
2024-02-05 14:44:02 -0700

    ini

---
+++ MauiApp1/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿namespace MauiApp1;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+
+        Label1.Text = """
+                      <strong>Bold Text</strong><br>
+                      <em>italics</em><br>
+                      <ul>
+                        <li>Item 1</li>
+                        <li>Item 2</li>
+                      </ul>
+                      """;
+    }
+}
